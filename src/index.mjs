677c7418099603295711b07ea0c139eccdc30f4b
Tiago Roos <tiago.roos@bagy.com.br>
2024-10-25 13:48:26 -0300

    commit inicial

---
+++ src/index.mjs
@@ -0,0 +1,45 @@
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PROTO_PATH =  path.join(__dirname, './proto/routes-guide.proto')
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+)
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+const routesGuide = protoDescriptor.RoutesGuide
+
+var client = new routesGuide('localhost:50051', grpc.credentials.createInsecure());
+
+client.getPerson({ id: 3 }, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+})
+
+const call = client.getPeople({ idBiggerThan: 1 })
+
+call
+    .on('data', (data) => {
+        console.log(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log('done');
+    })
